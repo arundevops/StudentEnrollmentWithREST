450bff2dee226b64ec64297243b9a047b888bc16
ganesh <ganeshva07@mail.com>
2017-01-23 23:46:39 -0800

    dd

--- src/main/java/com/github/elizabetht/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.github.elizabetht.service;
-
-import com.github.elizabetht.model.StudentInterface;
-
-public interface StudentService {
-	StudentInterface save(StudentInterface student);
-	boolean findByLogin(String userName, String password);
-	boolean findByUserName(String userName);
-}
